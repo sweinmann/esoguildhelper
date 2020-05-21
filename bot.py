66b66c1a75dabdd275364c0f8ac7e02d169f478a
Scott Weinmann <weinmann.scott@gmail.com>
2020-05-21 15:30:18 -0700

    Added dad joke to the bot

--- bot.py
+++ bot.py
@@ -15,20 +15,22 @@ if __name__ == "__main__":
     async def on_message(message):
         if message.content.find("!help") != -1:
             await message.channel.send("Help is on the way..")
+
         if message.content.find("!author") != -1:
             await message.channel.send("Dub created me.")
-        if messgae.content.find("!whydadjoke") != -1:
-            await message.channel.send("Annabanana of course.")
-        if messsage.content.find("!dadjoke") != -1:
-            conn = http.client.HTTPSConnection("dad-jokes.p.rapidapi.com")
-            headers = {
-                'x-rapidapi-host': "dad-jokes.p.rapidapi.com",
-                'x-rapidapi-key' : "0ad692458cmsh7fec2bdb1575ca3p19c718jsne9f90cc31d76"
-            }
-            conn.request("GET", "random/jokes", headers=headers)
 
-            res=conn.getresponse()
-            joke=res.read()
-            await message.channel.send(joke.decode("utf-8"))
+        if message.content.find("!whydadjoke") != -1:
+            await message.channel.send("Annabanana of course.")
+
+        if message.content.find("!dadjoke") != -1:
+             conn = http.client.HTTPSConnection("icanhazdadjoke.com")
+             headers = {
+                 "Accept"      : "text/plain"
+             }
+             conn.request("GET", "/", headers=headers)
+             res = conn.getresponse()
+             data = res.read().decode("ascii")
+
+             await message.channel.send(data)
 
     client.run(token)
