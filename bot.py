a030b80423651cfc25578a038803fb5d6874a02b
Scott Weinmann <weinmann.scott@gmail.com>
2020-05-21 13:11:06 -0700

    Added dad joke

--- bot.py
+++ bot.py
@@ -1,5 +1,7 @@
 import discord
 import configparser
+import http.client
+
 
 if __name__ == "__main__":
 
@@ -15,5 +17,18 @@ if __name__ == "__main__":
             await message.channel.send("Help is on the way..")
         if message.content.find("!author") != -1:
             await message.channel.send("Dub created me.")
+        if messgae.content.find("!whydadjoke") != -1:
+            await message.channel.send("Annabanana of course.")
+        if messsage.content.find("!dadjoke") != -1:
+            conn = http.client.HTTPSConnection("dad-jokes.p.rapidapi.com")
+            headers = {
+                'x-rapidapi-host': "dad-jokes.p.rapidapi.com",
+                'x-rapidapi-key' : "0ad692458cmsh7fec2bdb1575ca3p19c718jsne9f90cc31d76"
+            }
+            conn.request("GET", "random/jokes", headers=headers)
+
+            res=conn.getresponse()
+            joke=res.read()
+            await message.channel.send(joke.decode("utf-8"))
 
     client.run(token)
