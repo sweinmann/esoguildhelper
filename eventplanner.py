c1a34b47c5a4c969f0bbd7919bc031181d1c8d8a
Scott Weinmann <weinmann.scott@gmail.com>
2020-05-26 12:37:44 -0700

    Added __pycache__ to the .gitignore file

---
+++ eventplanner.py
@@ -0,0 +1,5 @@
+import datetime
+
+class eventplanner (object):
+    def __init__(self, **kwargs):
+        self.__dict__.update(**kwargs)
---
+++ test.py
@@ -0,0 +1,5 @@
+from eventplanner import *
+
+if __name__ == "__main__":
+    myargs = {'test' : 'drive', 'second': 'value'}
+    myobj = eventplanner(**myargs)
